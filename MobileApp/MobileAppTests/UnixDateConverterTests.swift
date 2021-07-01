02b3757b099d903d5877f1b9a4368cb533b860d0
Grzegorz Gumieniak <kontaktg.gumieniak@gmail.com>
2021-07-01 13:06:48 +0200

    Create setUp tests to application

---
+++ MobileApp/MobileAppTests/UnixDateConverterTests.swift
@@ -0,0 +1,19 @@
+//
+//  UnixDateConverterTests.swift
+//  ProjektManhatannTests
+//
+//  Created by Grzegorz Gumieniak on 01/07/2021.
+//
+
+import XCTest
+@testable import ProjektManhatann
+
+class UnixDateConverterTests: XCTestCase {
+    var unixDateConverter: UnixDateConverter!
+    override class func setUp() {
+        super.init()
+        unixDateConverter = UnixDateConverter()
+    }
+    
+    
+}
