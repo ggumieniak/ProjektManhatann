49fdeb94a9ffa8afcdd0392877414217ce0ceb2b
Grzegorz Gumieniak <kontaktg.gumieniak@gmail.com>
2021-07-01 13:03:46 +0200

    Create struct which convert data from UnixTime to representational in string

---
+++ MobileApp/MobileApp/Utilities/UnixDateConverter.swift
@@ -0,0 +1,29 @@
+//
+//  UnixDateConverter.swift
+//  ProjektManhatann
+//
+//  Created by Grzegorz Gumieniak on 01/07/2021.
+//
+
+import Foundation
+
+struct UnixDateConverter {
+    
+    func convertUnixDateToStringDate(from dateInUnix: Int) -> String {
+        return getDateInString(from: dateInUnix)
+    }
+    
+    private func getDateInString(from dateInInt: Int) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/YY"
+        return dateFormatter.string(from: createDateInInt(from: dateInInt))
+    }
+    
+    private func createDateInInt(from dateInInt: Int) -> Date {
+        return Date(timeIntervalSince1970: createTimeIntervalFromInt(dateInInt))
+    }
+    
+    private func createTimeIntervalFromInt(_ dateInInt: Int) -> TimeInterval {
+        return TimeInterval(dateInInt)
+    }
+}
